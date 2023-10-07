b16081109b595fb05f267a35ad7172bce27eb2ba
Cierra_Runis <2864283875@qq.com>
2023-10-07 23:33:45 +0800

    📖Add extensions

---
+++ lib/common/extension.dart
@@ -0,0 +1,20 @@
+import 'package:volo_meeting/index.dart';
+
+extension PlatformExtension on Platform {
+  static final isMobile = Platform.isAndroid || Platform.isIOS;
+}
+
+extension BuildContextExtension on BuildContext {
+  Future<T?> push<T extends Object?>(Widget page) =>
+      Navigator.push(this, CupertinoPageRoute<T>(builder: (context) => page));
+
+  void pop<T extends Object?>([T? result]) => Navigator.pop(this, result);
+
+  ColorScheme get colorScheme => Theme.of(this).colorScheme;
+  Brightness get brightness => colorScheme.brightness;
+}
+
+extension BrightnessExtension on Brightness {
+  bool get isDark => this == Brightness.dark;
+  bool get isLight => this == Brightness.light;
+}
