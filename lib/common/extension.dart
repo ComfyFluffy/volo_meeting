861d92122661dd25df2f443c2a88ba6d932c8b83
Cierra_Runis <2864283875@qq.com>
2023-10-08 23:29:01 +0800

    📖Add extension

--- lib/common/extension.dart
+++ lib/common/extension.dart
@@ -7,9 +7,16 @@ extension PlatformExtension on UniversalPlatform {
 
 extension BuildContextExtension on BuildContext {
   Future<T?> push<T extends Object?>(Widget page) =>
-      Navigator.push(this, CupertinoPageRoute<T>(builder: (context) => page));
+      Navigator.push(this, CupertinoPageRoute<T>(builder: (_) => page));
 
   void pop<T extends Object?>([T? result]) => Navigator.pop(this, result);
+
+  Future<T?> pushAndRemoveRoot<T extends Object?>(Widget page) =>
+      Navigator.pushAndRemoveUntil(
+        this,
+        CupertinoPageRoute<T>(builder: (_) => page),
+        (_) => false,
+      );
 
   ColorScheme get colorScheme => Theme.of(this).colorScheme;
   Brightness get brightness => colorScheme.brightness;
