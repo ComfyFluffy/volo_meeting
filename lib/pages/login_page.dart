11e6c57941e69ba06ce181d56fe30536c4554034
ComfyFluffy <24245520+ComfyFluffy@users.noreply.github.com>
2023-10-10 00:16:25 +0800

    update login page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -6,10 +6,10 @@ class LoginPage extends ConsumerStatefulWidget {
   });
 
   @override
-  ConsumerState<LoginPage> createState() => _UsernamePageState();
+  ConsumerState<LoginPage> createState() => _LoginPageState();
 }
 
-class _UsernamePageState extends ConsumerState<LoginPage> {
+class _LoginPageState extends ConsumerState<LoginPage> {
   late final _controller = TextEditingController(
     text: ref.watch(settingsProvider).username,
   );
@@ -18,36 +18,44 @@ class _UsernamePageState extends ConsumerState<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('用户名'),
+        title: const Text('登录'),
       ),
-      body: BasedListView(
-        children: [
-          BasedListSection(
-            titleText: '请设置用户名',
-            children: [
-              Center(
-                child: TextField(
+      body: Center(
+        child: Container(
+          constraints: const BoxConstraints(
+            maxWidth: 400,
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              children: [
+                TextField(
                   controller: _controller,
+                  decoration: const InputDecoration(
+                    labelText: '用户名',
+                    border: OutlineInputBorder(),
+                  ),
                 ),
-              ),
-            ],
+                const SizedBox(height: 32),
+                FilledButton(
+                  onPressed: () {
+                    if (_controller.text.isNotBlank) {
+                      ref
+                          .read(settingsProvider.notifier)
+                          .setUsername(_controller.text);
+                      context.pushAndRemoveRoot(const RootPage());
+                    } else {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(content: Text('用户名不能为空')),
+                      );
+                    }
+                  },
+                  child: const Text('登录'),
+                ),
+              ],
+            ),
           ),
-          TextButton(
-            onPressed: () {
-              if (_controller.text.isNotBlank) {
-                ref
-                    .read(settingsProvider.notifier)
-                    .setUsername(_controller.text);
-                context.pushAndRemoveRoot(const RootPage());
-              } else {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(content: Text('用户名不能为空')),
-                );
-              }
-            },
-            child: const Text(''),
-          ),
-        ],
+        ),
       ),
     );
   }
